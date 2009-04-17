c874639246a44d2ee3ba8dffcb37465407a82ee5
Serabe <serabe@gmail.com>
2009-04-17 23:29:56 +0200

    Added a spec for too many arguments. Remove a vim swp file.

--- spec/signature_spec.rb
+++ spec/signature_spec.rb
@@ -1,10 +1,18 @@
 require "spec_helper"
 
 describe Signature do
-  it "should raise an exception if fewer arguments than needed are passed" do
+  it "should raise an exception if less arguments than needed are given" do
     lambda{
       class TestClass
         signature "method2100", [] => Java::void
+      end
+    }.should raise_error(ArgumentError)
+  end
+
+  it "should raise an exception if more parameters than needed are given" do
+    lambda{
+      class TestClass
+        signature "method0000", [java.lang.String] => Java::void
       end
     }.should raise_error(ArgumentError)
   end
