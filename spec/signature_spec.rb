b683663591f4195b836c92e39484cc4878b5465b
Serabe <serabe@gmail.com>
2009-04-18 00:03:27 +0200

    Refactored some specs.

--- spec/signature_spec.rb
+++ spec/signature_spec.rb
@@ -1,19 +1,21 @@
 require "spec_helper"
 
 describe Signature do
-  it "should raise an exception if less arguments than needed are given" do
-    lambda{
-      class TestClass
-        signature "method2100", [] => Java::void
-      end
-    }.should raise_error(ArgumentError)
-  end
+  describe "should raise an exception if" do
+    it "less arguments than needed are given" do
+      lambda{
+        class TestClass
+          signature "method2100", [] => Java::void
+        end
+      }.should raise_error(ArgumentError)
+    end
 
-  it "should raise an exception if more parameters than needed are given" do
-    lambda{
-      class TestClass
-        signature "method0000", [java.lang.String] => Java::void
-      end
-    }.should raise_error(ArgumentError)
+    it "more arguments than the method can accept are given" do
+      lambda{
+        class TestClass
+          signature "method0000", [java.lang.String] => Java::void
+        end
+      }.should raise_error(ArgumentError)
+    end
   end
 end
