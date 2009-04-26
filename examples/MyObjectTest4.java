1c71f295153c5d746ae0fb288be07f304c2c50ac
Serabe <serabe@gmail.com>
2009-04-26 05:12:05 +0200

    Deleted two useless examples and changed version to a more epsilonic one.

--- examples/MyObjectTest4.java
+++
@@ -1,9 +0,0 @@
-public class MyObjectTest4 {
-  public static void main(String[] args) {
-    MyObject obj = new MyObject();
-    obj.helloWorld();
-    obj.byeBye(", bye!", "Youth");
-    obj.goodbyeWorld(" rocks!");
-    obj.goodbyeWorld(" rocks!", "Compiler2");
-  }
-}
--- src/version.rb
+++ src/version.rb
@@ -1,3 +1,3 @@
 class Ruby2JavaInfo
-  VERSION='0.1.0'
+  VERSION='0.0.1'
 end
